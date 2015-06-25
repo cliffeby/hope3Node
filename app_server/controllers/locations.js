febccbb10e1e9a820fa52b4ccb8d0b44e52bfeb7
cliffeby <clifford.eby@gmail.com>
2015-06-24 22:14:32 -0400

    Creating Hope

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,30 +1,24 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', {
-        title: 'Loc8r - find a place to work with wifi',
+    res.render('category-list', {
+        title: 'Hope Express - Home',
         pageHeader: {
-            title: 'Loc8r',
-            strapline: 'Find places to work with wifi near you!'
+            title: 'HOPE',
+            strapline: 'Create a inventory app!'
         },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
+        sidebar: "Anything for a sidebar?",
+        hopeCategories: [{
+            name: 'Clothing',
+            perishable: 'no',
+            bin: 3,
+            location: '100 Main St.',
+            value: '$10'
         }, {
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-        }, {
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
+            name: 'Food',
+            perishable: 'yes',
+            bin: 13,
+            location: '100 Main St.',
+            value: '$5'
         }]
     });
 };
@@ -32,61 +26,29 @@ module.exports.homelist = function(req, res) {
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
     res.render('location-info', {
-        title: 'Starcups',
+        title: 'Hope Home',
         pageHeader: {
-            title: 'Starcups'
+            title: 'HOPE Office location'
         },
         sidebar: {
-            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+            context: 'sidebar contex'
         },
         location: {
-            name: 'Starcups',
+            name: 'Main Office',
             address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            coords: {
-                lat: 51.455041,
-                lng: -0.9690884
-            },
-            openingTimes: [{
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
-            }, {
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
-            }, {
-                days: 'Sunday',
-                closed: true
-            }],
-            reviews: [{
-                author: 'Simon Holmes',
-                rating: 5,
-                timestamp: '16 July 2013',
-                reviewText: 'What a great place. I can\'t say enough good things about it.'
-            }, {
-                author: 'Charlie Chaplin',
-                rating: 3,
-                timestamp: '16 June 2013',
-                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-            }]
         }
     });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {
-        title: 'Review Starcups on Loc8r',
+    res.render('item-create-form', {
+        title: 'Create item in category',
         pageHeader: {
-            title: 'Review Starcups'
+            title: 'Item'
         },
         user: {
-            displayName: "Simon Holmes"
+            displayName: "Cliff Eby"
         }
     });
 };
