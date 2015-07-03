2508c9d74d16aecfa909c93b773d375c0d4e8173
Cliff Eby <ebyc@pbworld.com>
2015-07-03 14:19:10 -0400

    Added grunt

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+/**
+ * Created by EbyC on 7/3/2015.
+ */
+/**
+ * Created by EbyC on 6/30/2015.
+ */
+/*global require, module,  __dirname */
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        nodemon: {
+            dev: {
+                script: "./bin/www"
+            }
+        },
+        jshint: {
+            src: ['gruntfile.js', './app_server/**/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    require: true,
+                    define: true,
+                    requirejs: true,
+                    describe: true,
+                    expect: true,
+                    it: true
+                }
+            }
+        },
+        eslint: {
+            target: ['./app_server/**/*.js']
+        }
+    });
+
+
+    // Load JSHint task
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    // Default task.
+    grunt.registerTask('default', [ 'jshint','nodemon']);
+    grunt.registerTask('lint', ['eslint']);
+
+
+};
